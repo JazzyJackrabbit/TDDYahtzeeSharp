57d0c42d7cedf1dbbd4a1edac7de96bf6cdf570f
cxims <ximenesclement@gmail.com>
2022-04-22 18:24:04 +0200

    dice test

--- yahtzee.test/DiceTest.cs
+++ yahtzee.test/DiceTest.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using yahtzeeapi.Services;
 
 namespace yahtzeeapi.test
 {
     public class DiceTest
     {
-    }
+		[Fact]
+		public void test()
+		{
+			Dice dice = new Dice();
+		}
+	}
 }
