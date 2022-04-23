24a939e48987cdc8fec737e4b9823df7475d0d90
cxims <ximenesclement@gmail.com>
2022-04-23 16:02:59 +0200

    dices test

--- yahtzee.test/DiceTest.cs
+++ yahtzee.test/DiceTest.cs
@@ -9,9 +9,27 @@ namespace yahtzeeapi.test
     public class DiceTest
     {
 		[Fact]
-		public void test()
+		public void DiceRolledShouldBeReturn()
 		{
 			Dice dice = new Dice();
+			dice.roll();
+
+			int roll = dice.getRoll();
+
+			Assert.NotEqual(0, roll);
+		}
+
+		[Fact]
+
+		public void DiceRollShouldBeBetween1To6()
+        {
+			Dice dice = new Dice();
+			dice.roll();
+			int roll = dice.getRoll();
+
+			Assert.True(roll >= 1 && roll <= 6);
 		}
 	}
+
+
 }
--- yahtzee/Services/Dice.cs
+++ yahtzee/Services/Dice.cs
@@ -7,6 +7,18 @@ namespace yahtzeeapi.Services
 {
     public class Dice
     {
-        public int value { get; set; }
+        public int value;
+
+        public int getRoll()
+        {
+            return value;
+        }
+
+        public void roll()
+        {
+            Random random = new Random();
+            value = random.Next(1, 7);
+        }
+            
     }
 }
